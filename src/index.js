ba892b23437d8d55583ea2d688e1266fc41b07ea
Luis <luis@gmail.com>
2026-05-07 00:07:46 -0600

    fix webhook

--- src/index.js
+++ src/index.js
@@ -8,29 +8,30 @@ const PORT = process.env.PORT || 10000;
 
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN || "sevensport123";
 
-// WEBHOOK VERIFICATION
+// Verificación webhook
 app.get("/webhook", (req, res) => {
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
 
-  if (mode && token === VERIFY_TOKEN) {
-    console.log("Webhook verificado");
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    console.log("WEBHOOK_VERIFIED");
     return res.status(200).send(challenge);
   }
 
   return res.sendStatus(403);
 });
 
-// RECEIVE MESSAGES
+// Recibir mensajes
 app.post("/webhook", (req, res) => {
+  console.log("Mensaje recibido:");
   console.log(JSON.stringify(req.body, null, 2));
 
   res.sendStatus(200);
 });
 
 app.get("/", (req, res) => {
-  res.send("Servidor funcionando 🚀");
+  res.send("Bot funcionando 🚀");
 });
 
 app.listen(PORT, () => {
